198ae0de573fda53d1189e8817d13274106846fe
Kenneth Pham <kenneth.pham@gmail.com>
2023-12-06 14:33:50 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express'); // using common js instead of es 2015
+
+const app = express(); // create a new app, express can support multiple apps
+
+// define route handlers
+app.get('/', (req, res) => { // http get method for route of context root /
+  res.send({ hi: 'there'});
+});
+
+// start the app and listening to the port
+const PORT = process.env.PORT || 5000; // the port number is 5000 or will be injected from environment settings by hosting
+
+app.listen(PORT); // localhost:5000
+
+// node index.js or check start script in the package.json file
