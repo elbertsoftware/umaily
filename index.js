ccee6639e9b0b42cda6f30d3a5e137a88ff986b6
Kenneth Pham <kenneth.pham@gmail.com>
2023-12-06 15:39:40 -0800

    Test redeployment automatically

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express(); // create a new app, express can support multiple apps
 
 // define route handlers
 app.get('/', (req, res) => { // http get method for route of context root /
-  res.send({ hi: 'there'});
+  res.send({ bye: 'buddy'});
 });
 
 // start the app and listening to the port
 const PORT = process.env.PORT || 5000; // the port number is 5000 or will be injected from environment settings by hosting
 
-app.listen(PORT); // localhost:5000
+app.listen(PORT); // localhost:5000 or render provided URL https://umaily.onrender.com
 
 // node index.js or check start script in the package.json file
