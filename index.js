c2a3dfcc7fb3d7762f93ccabeee2779644969ddd
Kenneth Pham <kenneth.pham@gmail.com>
2023-12-10 04:25:02 -0800

    Added Google OAuth 2.0

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express'); // using common js instead of es 2015
 
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express(); // create a new app, express can support multiple apps
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 // define route handlers
+
+/*
+// simplest test route
 app.get('/', (req, res) => { // http get method for route of context root /
   res.send({ bye: 'buddy'});
 });
+/**/ 
 
 // start the app and listening to the port
 const PORT = process.env.PORT || 5000; // the port number is 5000 or will be injected from environment settings by hosting
